0a25cf0a0c26a516e66784b7a3eb92adb587b08d
vucaoit <86138777+vucaoit@users.noreply.github.com>
2023-04-05 11:02:23 +0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by vdcao on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
